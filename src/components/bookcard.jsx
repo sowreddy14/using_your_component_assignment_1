427ceeba3216f36947ed621545a4b73cd317d782
Harry <harikrishnasowreddy14@gmail.com>
2025-03-20 14:08:33 +0530

    changes done

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,16 @@
-// write the book component code here
+//components/BookCard.jsx
+import React from "react";
+import "./BookCard.css"; // Import CSS file
+
+const BookCard = ({ image, name, genre, author }) => {
+  return (
+    <div className="book-card">
+      <img src={image} alt={name} className="book-image" />
+      <h2 className="book-title">{name}</h2>
+      <p className="book-genre">Genre: {genre}</p>
+      <p className="book-author">By {author}</p>
+    </div>
+  );
+};
+
+export default BookCard;
